91c57052070fd00c609880a5c151c5ece68e8893
David <davidsim1035@gmail.com>
2025-03-26 16:34:30 +0100

    Added user with prisma client and prisma studio in db

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express();
 const prisma = new PrismaClient();
 app.use(express.json());
 
+async function main() {
+  const newUser = await prisma.user.create({
+    data: { email: "johndoe@test.fr", name: "John Doe" },
+  });
+  console.log(newUser);
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+main().catch((e) => console.error(e));
+
 // Get all users
 app.get("/", async (req, res) => {
   const userCount = await prisma.user.count();
