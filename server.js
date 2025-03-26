9bc48d8bf8db646f6114bba9b0440c2ff9c0f496
David <davidsim1035@gmail.com>
2025-03-26 15:49:25 +0100

    Dockerizing our app and added Postgres, Adminer and Prisma

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-import express from "express";
-
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 const app = express();
+const prisma = new PrismaClient();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "App is running on docker container" });
+// Get all users
+app.get("/", async (req, res) => {
+  const userCount = await prisma.user.count();
+  res.json(
+    userCount == 0
+      ? "No users have been added yet."
+      : "Sonme users have been added to the database."
+  );
 });
 
+const PORT = 3000;
+
 app.listen(PORT, () => {
-  console.log(`App is running on ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
